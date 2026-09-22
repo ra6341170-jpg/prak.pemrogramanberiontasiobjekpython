7f4eb0dee1f3d05bacdd5f2e1e042d7a73c79e7d
Rizky Ardiansyah <ra6341170@gmail.com>
2026-09-22 17:37:42 +0700

    Add files via upload

---
+++ latihan/per3.py
@@ -0,0 +1,42 @@
+# 1
+print(5 + 3 * 4)  # 17
+
+# 2
+print((5 + 3) * 4)  # 32
+
+# 3
+print(18 / 4)  # 4.5
+
+# 4
+print(18 // 4)  # 4
+
+# 5
+print(18 % 4)  # 2
+
+# 6
+print(3 ** 4)  # 81
+
+# 7
+print(10 + 2 * 3 - 4 / 2)  # 14.0
+
+# 8
+a = 15
+b = 4
+print(a // b + a % b * 2)  # 9
+
+# 9
+print(int("20") + float("3.5") * 2)  # 27.0
+
+# 10
+panjang = 12
+lebar = 5.5
+print(panjang * lebar)  # 66.0
+
+# 11
+harga = 200000
+diskon = 0.1
+print(harga - (harga * diskon))  # 180000.0
+
+# 12
+nilai = [85, 90, 78]
+print(sum(nilai) / len(nilai))  # 84.33333333333333
